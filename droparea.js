c2fc3fd17d9d57db0f15d7b145d7b9d6bcd3387f
kmori <mori.dev.asdf@gmail.com>
2011-06-29 13:06:49 +0900

    少しリファクタ

--- droparea.js
+++ droparea.js
@@ -1,124 +1,135 @@
-(function( $ ){
-    var s;
-    // Methods
-    var m = {
-        init: function(e){},
-        start: function(e){},
-        complete: function(r){},
-        error: function(r){ alert(r.error); return false; },
-        traverse: function(files,area){
-            if (typeof files !== "undefined") {
-                for (var i=0, l=files.length; i<l; i++) {
-                    m.upload(files[i], area);
-                }
-            } else {
-                area.html(nosupport);
-            }
-        },
-        upload: function(file, area){
-            //area.empty();
-            var progress = $('<div>',{
-                'class':'progress'
-            });
-            area.append(progress);
-			
-            // File type control
-            if (typeof FileReader === "undefined" || !(/image/i).test(file.type)) {
-                //area.html(file.type,s.noimage);
-                alert('only image files: jpeg, png, gif');
-                return false;
-            }
+(function($){
 
-            // File size control
-            if (file.size > (s.maxsize * 1024)) {
-                //area.html(file.type,s.maxsize);
-                alert('max upload size: ' + s.maxsize + 'Kb');
-                return false;
-            }
-			
-            // Uploading - for Firefox, Google Chrome and Safari
-            var xhr = new XMLHttpRequest();
-            // Update progress bar
-            xhr.upload.addEventListener("progress", function (e) {
-                if (e.lengthComputable) {
-                    var loaded = Math.ceil((e.loaded / e.total) * 100) + "%";
-                    progress.css({
-                        'height':loaded
-                    }).html(loaded);
-                }
-            }, false);
-			
-            // File uploaded
-            xhr.addEventListener("load", function (e) {
-                var r = jQuery.parseJSON(e.target.responseText);
-                s.complete(r);
-                area.find('img').remove();
-                area.data('value',r.filename)
-                .append($('<img>',{'src': r.path + r.filename + '?' + Math.random()}));
-                progress.addClass('uploaded');
-                progress.html(s.uploaded).fadeOut('slow');
-            }, false);
-			
-            xhr.open("POST", s.post, true);
-            
-            // Set appropriate headers
-            xhr.setRequestHeader("Content-Type", "multipart/form-data");
-            xhr.setRequestHeader("x-file-name", file.fileName);
-            xhr.setRequestHeader("x-file-size", file.fileSize);
-            xhr.setRequestHeader("x-file-type", file.type);
+    $.fn.droparea = function(user_config) {
 
-            // Set request headers
-            for (var i in area.data())
-                if (typeof area.data(i) !== "object")
-                    xhr.setRequestHeader('x-param-'+i, area.data(i));
+       var methods = {
 
-            xhr.send(file);
-        }
-    };
-    $.fn.droparea = function(o) {
-        // Settings
-        s = {
-            'init': m.init,
-            'start': m.start,
-            'complete': m.complete,
-            'instructions': 'drop an image file here',
-            'over'        : 'drop file here!',
-            'nosupport'   : 'No support for the File API in this web browser',
-            'noimage'     : 'Unsupported file type!',
-            'uploaded'    : 'Uploaded',
-            'maxsize'     : '500', //Kb
-            'post'        : 'upload.php'
+           init     : function(e){},
+           start    : function(e){},
+           complete : function(r){},
+           error    : function(r){ alert(r.error); return false; },
+
+           traverse : function(files, area){
+               if (typeof files !== 'undefined') {
+                   for (var i=0, l=files.length; i<l; i++) {
+                       methods.upload(files[i], area);
+                   }
+               } else {
+                   area.html(nosupport);
+               }
+           },
+
+           upload: function(file, area) {
+
+               // File type control
+               if (typeof FileReader === 'undefined' || !(/image/i).test(file.type)) {
+                   //area.html(file.type, dnd.noimage);
+                   alert('jpeg, png, gif に対応しています');
+                   return false;
+               }
+
+               // File size control
+               if (file.size > (dnd.maxsize * 1024)) {
+                   //area.html(file.type,dnd.maxsize);
+                   alert('max upload size: ' + dnd.maxsize + 'Kb');
+                   return false;
+               }
+
+               // Uploading - for Firefox, Google Chrome and Safari
+               var xhr = new XMLHttpRequest();
+
+               // File uploaded
+               xhr.addEventListener('load', function (e) {
+
+                   var r = jQuery.parseJSON(e.target.responseText);
+
+                   dnd.complete(r);
+                   area.find('img').remove();
+                   area.data('value', r.filename)
+                     .append($('<img>', {'src': r.path + r.filename + '?' + Math.random()}));
+
+               }, false);
+
+               xhr.open('POST', dnd.post, true);
+
+               // Set appropriate headers
+               xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+               xhr.setRequestHeader('x-file-name', file.fileName);
+               xhr.setRequestHeader('x-file-size', file.fileSize);
+               xhr.setRequestHeader('x-file-type', file.type);
+
+               // Set request headers
+               for (var i in area.data()) {
+                   if (typeof area.data(i) !== 'object') {
+                       xhr.setRequestHeader('x-param-'+i, area.data(i));
+                   }
+               }
+
+               xhr.send(file);
+
+           }
+       };
+
+       var dnd = {
+            'init'         : methods.init,
+            'start'        : methods.start,
+            'complete'     : methods.complete,
+            'error'        : methods.error,
+            'instructions' : 'ここにファイルをドロップして下さい',
+            'over'         : 'ここはドロップできる領域です',
+            'nosupport'    : 'File API に対応していません',
+            'noimage'      : 'サポートしていないフォーマットです',
+            'uploaded'     : 'アップロード完了',
+            'maxsize'      : '50000', //Kb
+            'post'         : './upload.php'
         };
-        this.each(function(){
-            if(o) $.extend(s, o);
-            var instructions = $('<div>').appendTo($(this));
-            s.init($(this));            
-            if(!$(this).data('value'))
-                instructions.addClass('instructions').html(s.instructions);
 
-            $(this)
-            .bind({
+
+        return this.each(function() {
+
+            if(user_config) {
+                $.extend({}, o, user_config);
+            }
+
+            var instructions = $('<div>').appendTo($(this));
+
+            dnd.init($(this));
+
+            if(!$(this).data('value')) {
+                instructions.addClass('instructions').html(dnd.instructions);
+            }
+
+            $(this).bind({
+
                 dragleave: function (e) {
                     e.preventDefault();
-                    if($(this).data('value'))
+                    if($(this).data('value')) {
                         instructions.removeClass().empty();
-                    else
-                        instructions.removeClass('over').html(s.instructions);
+                    } else {
+                        instructions.removeClass('over').html(dnd.instructions);
+                    }
                 },
+
                 dragenter: function (e) {
                     e.preventDefault();
-                    instructions.addClass('instructions over').html(s.over);
+                    instructions.addClass('instructions over').html(dnd.over);
                 },
+
                 dragover: function (e) {
                     e.preventDefault();
                 }
+
             });
-            this.addEventListener("drop", function (e) {
+
+            this.addEventListener('drop', function (e) {
+
                 e.preventDefault();
-                s.start($(this));
-                m.traverse(e.dataTransfer.files, $(this));
+                dnd.start($(this));
+                methods.traverse(e.dataTransfer.files, $(this));
                 instructions.removeClass().empty();
-            },false);
+
+            }, false);
+
         });
     };
-})( jQuery );
+})(jQuery);
